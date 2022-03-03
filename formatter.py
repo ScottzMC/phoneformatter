dd3e06921d65e40d8f31967a2b8ab042133fdcd7
ScottzMC <scottmike275@gmail.com>
2022-03-03 13:48:08 +0000

    Added Unit test

--- formatter.py
+++ formatter.py
@@ -1,36 +1,37 @@
 # Formatter Module
 
-# Function checks if it is a UK mobile number
-def area_code_for_gb(mobile_number):
+# Function checks if it is a UK number
+def check_phone_number_for_uk(uk_number):
     try:
-        if mobile_number[0] == "0":
-            # Checks if there are any letters found in the user input
-            for char in mobile_number:
-                # Returns an error if a letter is found
-                if char.isalpha():
-                    print("Mobile number is incorrect, has invalid characters")
-                    exit()
-            # Adding the area code to the mobile number
-            mobile_number = mobile_number.replace(mobile_number[0], '+44', 1)
-            return mobile_number
+        number_list = ['01', '02', '07']
+        if uk_number[:2] in number_list:
+            return uk_number
         else:
-            print("Sorry, this number is not valid in the UK")
+            print("Sorry, this phone number is not valid in the UK")
             exit()
     except IndexError:
         print("Error Occurred! Please Input a mobile number")
         exit()
 
 # Function checks if the number is a valid mobile number
-def validate_number(mobile_number):
-    mobile_number_list = ['01', '02', '03', '04', '05', '06', '08', '09']
-    # Checks an array if the input is a mobile number
+def validate_mobile_number(mobile_number):
     try:
+        # Checks an array if the input is found
+        mobile_number_list = ['01', '02']
+        # Returns an error if the number is found in the array
         if mobile_number[:2] in mobile_number_list:
             print("Sorry, this is not a mobile number")
             exit()
         else:
+            # If found then it would clear and spaces
             mobile_number = mobile_number.replace(" ", "")
             return mobile_number
     except TypeError:
         print("Error Occurred! mobile number not in array")
         exit()
+
+# Function adds the area code number for the UK
+def add_area_code_for_uk(uk_number):
+    # Adding the area code to the mobile number
+    uk_number = uk_number.replace(uk_number[0], '+44', 1)
+    return uk_number
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import formatter as fm
+
 try:
-    input_number = input("Enter your mobile number: ")
+    phone_number = input("Enter your mobile number: ")
+    # Checks if the Phone number is valid in the UK
+    mobile_number = fm.check_phone_number_for_uk(phone_number)
     # Validates to ensure it is a mobile number
-    number = fm.validate_number(input_number)
-    # Trims any whitespace in the phone number
-    mobile_number = fm.area_code_for_gb(number)
+    mobile_number = fm.validate_mobile_number(mobile_number)
+    # Adds the area code for the mobile number
+    mobile_number = fm.add_area_code_for_uk(mobile_number)
     # Limit the mobile number to 11 digits
     print('UK Mobile number: ' + mobile_number[:13])
+
 except (ImportError, NameError, TypeError):
     print("Error Occurred!")
     exit()
