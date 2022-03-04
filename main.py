ae829005344564201aa5d3ea013a7ff6ca69cc6f
ScottzMC <scottmike275@gmail.com>
2022-03-04 09:18:17 +0000

    Update formatter

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import formatter as fm
+import formatter
 
 try:
     phone_number = input("Enter your mobile number: ")
     # Checks if the Phone number is valid in the UK
-    mobile_number = fm.check_phone_number_for_uk(phone_number)
+    mobile_number = formatter.check_phone_number_for_uk(phone_number)
     # Validates to ensure it is a mobile number
-    mobile_number = fm.validate_mobile_number(mobile_number)
+    mobile_number = formatter.validate_mobile_number(mobile_number)
     # Adds the area code for the mobile number
-    mobile_number = fm.add_area_code_for_uk(mobile_number)
+    mobile_number = formatter.add_area_code_for_uk(mobile_number)
     # Limit the mobile number to 11 digits
     print('UK Mobile number: ' + mobile_number[:13])
 
--- test_formatter.py
+++ test_formatter.py
@@ -1,5 +1,5 @@
 import pytest
-import formatter as fm
+import formatter
 
 class TestFormatter:
     number_list = ['01', '02', '07']
@@ -9,15 +9,15 @@ class TestFormatter:
 
     # Test the Check phone number function is valid in the UK
     def test_check_phone_number_for_uk(self):
-        assert fm.check_phone_number_for_uk(self.phone_number) in self.number_list
+        assert formatter.check_phone_number_for_uk(self.phone_number) in self.number_list
 
     # Test to Validate mobile number function
     def test_validate_mobile_number(self):
-        assert fm.validate_mobile_number(self.phone_number) == self.test_mobile_number
+        assert formatter.validate_mobile_number(self.phone_number) == self.test_mobile_number
 
     # Test the Add area code for Uk function
     def test_add_area_code_for_uk(self):
-        assert fm.add_area_code_for_uk(self.mobile_number)
+        assert formatter.add_area_code_for_uk(self.mobile_number)
 
 if __name__ == '__main__':
     pytest.main()
